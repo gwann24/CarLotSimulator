830744eab8145909805e4802dac5eb3f8ea46f74
Greg Wanner <greg@tegogroup.com>
2024-02-26 17:09:47 -0500

    Modified code to instanciate first 3 cars

---
+++ CarLotSimulatorApp/CarLotSimulator/Cars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDrivable = isDrivable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+        public void MakeEngineNoise(string sound)
+        {
+            Console.WriteLine($"Engine makes a {sound} noise.");
+        }
+        public void MakeHornNoise(string sound)
+        {
+            Console.WriteLine($"Horn goes {sound}.");
+        }
+
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Cars;
+using Microsoft.VisualBasic;    
 
 namespace CarLotSimulator
 {
@@ -16,6 +18,31 @@ namespace CarLotSimulator
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
+            Car Car1 = new Car();
+            Car1.Year = 2023;
+            Car1.Make = "Telsa";
+            Car1.Model = "Model 3";
+            Car1.EngineNoise = "Whirring";
+            Car1.HonkNoise = "Beep";
+            Car1.IsDrivable = true;
+
+            Console.WriteLine($"A {Car1.Year} {Car1.Make} {Car1.Model}:");
+            Car1.MakeEngineNoise(Car1.EngineNoise);
+            Car1.MakeHornNoise(Car1.HonkNoise);
+            Console.WriteLine("\n");
+
+            Car Car2 = new Car() { Year = 1968, Make = "Chevy", Model = "Covette", EngineNoise = "Vroom", HonkNoise = "Honk", IsDrivable = true };
+            Console.WriteLine($"A {Car2.Year} {Car2.Make} {Car2.Model}:");
+            Car2.MakeEngineNoise(Car2.EngineNoise);
+            Car2.MakeHornNoise(Car2.HonkNoise);
+            Console.WriteLine("\n");
+
+            Car Car3 = new Car(1969, "Dodge", "Charger", "Roaring", "Dixie", true);
+            Console.WriteLine($"A {Car3.Year} {Car3.Make} {Car3.Model}:");
+            Car3.MakeEngineNoise(Car3.EngineNoise);
+            Car3.MakeHornNoise(Car3.HonkNoise);
+            Console.WriteLine("\n");
+
             //Call each of the methods for each car
 
             //*************BONUS*************//
